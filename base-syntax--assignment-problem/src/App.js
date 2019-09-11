3adb1d0d9240f0777d8f7bcba0264ac6db313134
DanialIdham <danialidhamyahya@gmail.com>
2019-09-11 17:11:37 +0800

    Commit

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
-
 class App extends Component {
-  state = {
-    username: 'supermax'
-  }
-
-  usernameChangedHandler = (event) => {
-    this.setState({username: event.target.value});
-  }
-
   render() {
     return (
       <div className="App">
@@ -28,12 +17,6 @@ class App extends Component {
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
-        <UserInput 
-          changed={this.usernameChangedHandler} 
-          currentName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName="Max" />
       </div>
     );
   }
